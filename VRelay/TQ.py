abe4f15aa6d8bd0e8495c85b97c0b7cae244c78c
Noting565 <wmrotmg@gmail.com>
2022-01-22 05:07:50 -0700

    minor polish

--- VRelay/TQ.py
+++ VRelay/TQ.py
@@ -9,237 +9,259 @@ import re
 import time
 import random
 
+
 class TQ(PluginInterface):
-    
-	hooks = {PacketTypes.PlayerText, PacketTypes.Update, PacketTypes.GotoAck, PacketTypes.UseItem}
-	load = True
-	defaultState = True
 
-	teleporting = False
-	listenToAbility = False
-	questPosition = WorldPosData()
-	noAck = False
-	tpPlayer = False
-	players = []
-	classTypes = [768, 775, 782, 784, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806,
-					 807, 808, 22566, 22570, 24896, 21945, 26306, 26307]
+    hooks = {PacketTypes.PlayerText, PacketTypes.Update,
+             PacketTypes.GotoAck, PacketTypes.UseItem}
+    load = True
+    defaultState = True
 
-	# Worlds
-	gardenFirst = False
-	gardenSecond = False
+    teleporting = False
+    listenToAbility = False
+    questPosition = WorldPosData()
+    noAck = False
+    tpPlayer = False
+    players = []
+    classTypes = [768, 775, 782, 784, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806,
+                  807, 808, 22566, 22570, 24896, 21945, 26306, 26307]
 
-	illuFirst = False
-	illuSecond = False
-	illuThird = False
+    # Worlds
+    gardenFirst = False
+    gardenSecond = False
 
-	shattersFirst = False
-	shattersSecond = False
-	shattersThird = False
+    illuFirst = False
+    illuSecond = False
+    illuThird = False
 
-	rift = False
+    shattersFirst = False
+    shattersSecond = False
+    shattersThird = False
 
-	def getAuthor(self):
-		return "Animus"
+    rift = False
 
-	def onUpdate(self, client: Client, packet: Update, send: bool) -> (Update, bool):
-		# Never always accurate 
-		if client.latestQuest != None:
-			try:
-				self.questPosition = client.newObjects[client.latestQuest].pos
-			except:
-				print('TeleportTools: Error, no quest avalible...')
-				# Add players that join
-		for obj in packet.newObjects:
-			for classType in self.classTypes:
-				for s in obj.objectStatusData.stats:
-					if obj.objectStatusData.objectID != client.objectID and obj.objectType == classType and s.statType == 31:
-						self.players.append(obj)
+    def getAuthor(self):
+        return "Animus"
 
-		# Remove players that have left
-		for d in packet.drops:
-			for p in self.players:
-				if d == p.objectStatusData.objectID:
-					self.players.remove(p)
-		return (packet, send)
-	
-	def onGotoAck(self, client: Client, packet: GotoAck, send: bool) -> (GotoAck, bool):
-		if self.teleporting == True:
-			if self.noAck == True:
-				self.teleporting = False
-				self.noAck = False
-				return (packet, False)
+    def onUpdate(self, client: Client, packet: Update, send: bool) -> (Update, bool):
+        # Never always accurate
+        if client.latestQuest != None:
+            try:
+                self.questPosition = client.newObjects[client.latestQuest].pos
+            except:
+                print('TeleportTools: Error, no quest avalible...')
+        # Add players that join
+        for obj in packet.newObjects:
+            for classType in self.classTypes:
+                for s in obj.objectStatusData.stats:
+                    if obj.objectStatusData.objectID != client.objectID and obj.objectType == classType and s.statType == 31:
+                        self.players.append(obj)
 
-			time.sleep(0.1)
-			g = Goto()
-			g.objectID = client.objectID
+        # Remove players that have left
+        for d in packet.drops:
+            for p in self.players:
+                if d == p.objectStatusData.objectID:
+                    self.players.remove(p)
+        return (packet, send)
 
-			pos = WorldPosData()
+    def onGotoAck(self, client: Client, packet: GotoAck, send: bool) -> (GotoAck, bool):
+        if self.teleporting == True:
+            if self.noAck == True:
+                self.teleporting = False
+                self.noAck = False
+                return (packet, False)
 
-			# Garden
-			if self.gardenFirst == True:
-				pos.x = 406
-				pos.y = 248
-				g.pos = pos
-				self.gardenFirst = False
+            time.sleep(0.1)
+            g = Goto()
+            g.objectID = client.objectID
 
-			elif self.gardenSecond == True:
-				pos.x = 406
-				pos.y = 325
-				g.pos = pos
-				self.gardenSecond = False
+            pos = WorldPosData()
 
-			# Illu
-			elif self.illuFirst == True:
-				pos.x = 25
-				pos.y = 40
-				g.pos = pos
-				self.illuFirst = False
+            # Garden
+            if self.gardenFirst == True:
+                pos.x = 406
+                pos.y = 248
+                g.pos = pos
+                self.gardenFirst = False
 
-			elif self.illuSecond == True:
-				pos.x = 130
-				pos.y = 20
-				g.pos = pos
-				self.illuSecond = False
+            elif self.gardenSecond == True:
+                pos.x = 406
+                pos.y = 325
+                g.pos = pos
+                self.gardenSecond = False
 
-			elif self.illuThird == True:
-				pos.x = 180
-				pos.y = 116
-				g.pos = pos
-				self.illuThird = False	
+            # Illu
+            elif self.illuFirst == True:
+                pos.x = 25
+                pos.y = 40
+                g.pos = pos
+                self.illuFirst = False
 
-			# Shatters
-			elif self.shattersFirst == True:
-				pos.x = 205
-				pos.y = 200
-				g.pos = pos
-				self.shattersFirst = False
-				
-			elif self.shattersSecond == True:
-				pos.x = 390
-				pos.y = 200
-				g.pos = pos
-				self.shattersSecond = False	
+            elif self.illuSecond == True:
+                pos.x = 130
+                pos.y = 20
+                g.pos = pos
+                self.illuSecond = False
 
-			elif self.shattersThird == True:
-				pos.x = 386
-				pos.y = 21
-				g.pos = pos
-				self.shattersThird = False	
+            elif self.illuThird == True:
+                pos.x = 180
+                pos.y = 116
+                g.pos = pos
+                self.illuThird = False
 
-			# Rift
-			elif self.rift == True:
-				pos.x = 19
-				pos.y = 93
-				g.pos = pos
-				self.rift = False			
-			else:
-				g.pos = self.questPosition
-			client.SendPacketToClient(CreatePacket(g))
-			self.noAck = True
+            # Shatters
+            elif self.shattersFirst == True:
+                pos.x = 205
+                pos.y = 200
+                g.pos = pos
+                self.shattersFirst = False
 
-		return (packet, send)
+            elif self.shattersSecond == True:
+                pos.x = 390
+                pos.y = 200
+                g.pos = pos
+                self.shattersSecond = False
 
-	def onUseItem(self, client: Client, packet: UseItem, send: bool) -> (UseItem, bool):
-		if self.listenToAbility == True:
-			packet.itemUsePos = client.currentpos
-			self.listenToAbility = False
-			self.teleporting = True
-			client.createNotification("TeleportTools", "Teleporting..")
+            elif self.shattersThird == True:
+                pos.x = 386
+                pos.y = 21
+                g.pos = pos
+                self.shattersThird = False
 
-		return (packet, send)	
+            # Rift
+            elif self.rift == True:
+                pos.x = 19
+                pos.y = 93
+                g.pos = pos
+                self.rift = False
+            else:
+                g.pos = self.questPosition
+            client.SendPacketToClient(CreatePacket(g))
+            self.noAck = True
 
-	def onPlayerText(self, client: Client, packet: PlayerText, send: bool) -> (PlayerText, bool):
-		if packet.text.lower() == "/tq":
-			if client.latestQuest != None:
-				self.listenToAbility = True
-				client.createNotification("TeleportTools", "Teleport to quest by using your ability")
-			else:
-				client.createNotification("TeleportTools", "No quest registered")
-			send = False
-		
-		# Garden
-		elif packet.text.lower() == "/garden 1":
-			if client.currentMap == "Garden of Chaos":
-				self.listenToAbility = True
-				self.gardenFirst = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in a Garden of Chaos!")		
-			send = False
+        return (packet, send)
 
-		elif packet.text.lower() == "/garden 2":
-			if client.currentMap == "Garden of Chaos":
-				self.listenToAbility = True
-				self.gardenSecond = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in a Garden of Chaos!")			
-			send = False
-	
-		# Illu
-		elif packet.text.lower() == "/illu 1":
-			if client.currentMap == "Crypt of the Illusionist":
-				self.listenToAbility = True
-				self.illuFirst = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in a Crypt of the Illusionist")
-			send = False	
+    def onUseItem(self, client: Client, packet: UseItem, send: bool) -> (UseItem, bool):
+        if self.listenToAbility == True:
+            packet.itemUsePos = client.currentpos
+            self.listenToAbility = False
+            self.teleporting = True
+            client.createNotification("TeleportTools", "Teleporting..")
 
-		elif packet.text.lower() == "/illu 2":
-			if client.currentMap == "Crypt of the Illusionist":
-				self.listenToAbility = True
-				self.illuSecond = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in a Crypt of the Illusionist")	
-			send = False			
+        return (packet, send)
 
-		elif packet.text.lower() == "/illu 3":
-			if client.currentMap == "Crypt of the Illusionist":
-				self.listenToAbility = True
-				self.illuThird = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")	
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in a Crypt of the Illusionist")	
-			send = False
+    def onPlayerText(self, client: Client, packet: PlayerText, send: bool) -> (PlayerText, bool):
+        if packet.text.lower() == "/tq":
+            if client.latestQuest != None:
+                self.listenToAbility = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to quest by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "No quest registered")
+            send = False
 
-		# Shatters
-		elif packet.text.lower() == "/shatters 1":
-			if client.currentMap == "The Shatters":
-				self.listenToAbility = True
-				self.shattersFirst = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")	
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in The Shatters")	
-			send = False
+        # Garden
+        elif packet.text.lower() == "/garden 1":
+            if client.currentMap == "Garden of Chaos":
+                self.listenToAbility = True
+                self.gardenFirst = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in a Garden of Chaos!")
+            send = False
 
-		elif packet.text.lower() == "/shatters 2":
-			if client.currentMap == "The Shatters":
-				self.listenToAbility = True
-				self.shattersSecond = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")		
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in The Shatters")	
-			send = False
+        elif packet.text.lower() == "/garden 2":
+            if client.currentMap == "Garden of Chaos":
+                self.listenToAbility = True
+                self.gardenSecond = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in a Garden of Chaos!")
+            send = False
 
-		elif packet.text.lower() == "/shatters 3":
-			if client.currentMap == "The Shatters":
-				self.listenToAbility = True
-				self.shattersThird = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")		
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in The Shatters")	
-			send = False		
+        # Illu
+        elif packet.text.lower() == "/illu 1":
+            if client.currentMap == "Crypt of the Illusionist":
+                self.listenToAbility = True
+                self.illuFirst = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in a Crypt of the Illusionist")
+            send = False
 
-		# Rift
-		elif packet.text.lower() == "/rift":
-			if client.currentMap == "Ascended Rift":
-				self.listenToAbility = True
-				self.rift = True
-				client.createNotification("TeleportTools", "Teleport to position by using your ability")	
-			else:
-				client.createNotification("TeleportTools", "You can only use this command in an Ascended Rift")	
-			send = False												
-		
-		return (packet, send)
+        elif packet.text.lower() == "/illu 2":
+            if client.currentMap == "Crypt of the Illusionist":
+                self.listenToAbility = True
+                self.illuSecond = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in a Crypt of the Illusionist")
+            send = False
+
+        elif packet.text.lower() == "/illu 3":
+            if client.currentMap == "Crypt of the Illusionist":
+                self.listenToAbility = True
+                self.illuThird = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in a Crypt of the Illusionist")
+            send = False
+
+        # Shatters
+        elif packet.text.lower() == "/shatters 1":
+            if client.currentMap == "The Shatters":
+                self.listenToAbility = True
+                self.shattersFirst = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in The Shatters")
+            send = False
+
+        elif packet.text.lower() == "/shatters 2":
+            if client.currentMap == "The Shatters":
+                self.listenToAbility = True
+                self.shattersSecond = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in The Shatters")
+            send = False
+
+        elif packet.text.lower() == "/shatters 3":
+            if client.currentMap == "The Shatters":
+                self.listenToAbility = True
+                self.shattersThird = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in The Shatters")
+            send = False
+
+        # Rift
+        elif packet.text.lower() == "/rift":
+            if client.currentMap == "Ascended Rift":
+                self.listenToAbility = True
+                self.rift = True
+                client.createNotification(
+                    "TeleportTools", "Teleport to position by using your ability")
+            else:
+                client.createNotification(
+                    "TeleportTools", "You can only use this command in an Ascended Rift")
+            send = False
+
+        return (packet, send)
--- VRelay/UnderageGambling.py
+++ VRelay/UnderageGambling.py
@@ -12,117 +12,128 @@ import random
 
 ''' This plugin is literally for trolling and nothing else. commands are listed in the player text hook on line 65 '''
 
+
 class UnderageGambling(PluginInterface):
 
-	hooks = {PacketTypes.Update, PacketTypes.PlayerText, PacketTypes.Hello, PacketTypes.Text}
-	load = True
-	defaultState = True
+    hooks = {PacketTypes.Update, PacketTypes.PlayerText,
+             PacketTypes.Hello, PacketTypes.Text}
+    load = True
+    defaultState = True
 
-	pluginName = "Underage Gambling"
-	debug = False
-	target = None
-	players = []
-	spamCount = 1
-	sleepCount = 0
-	classTypes = [768, 775, 782, 784, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806,
-					 807, 808, 22566, 22570, 24896, 21945, 26306, 26307]
+    pluginName = "Underage Gambling"
+    debug = False
+    target = None
+    players = []
+    spamCount = 1
+    sleepCount = 0
+    classTypes = [768, 775, 782, 784, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806,
+                  807, 808, 22566, 22570, 24896, 21945, 26306, 26307]
 
-	def getAuthor(self):
-		return "Animus"
+    def getAuthor(self):
+        return "Animus"
 
-	# Clear our list when we change a map.
-	def onHello(self, client: Client, packet: Hello, send: bool) -> (Hello, bool):
-		self.players.clear()
-		return (packet, send)
+    # Clear our list when we change a map.
+    def onHello(self, client: Client, packet: Hello, send: bool) -> (Hello, bool):
+        self.players.clear()
+        return (packet, send)
 
-	# Stop spam command from sending to ourselves ourselves if spam count > 1
-	def onText(self, Client: Client, packet: Text, send: bool) -> (Text, bool):
-		if self.spamCount > 1 and packet.text[0:22] == "You have sent a gamble":
-			send = False
-		return (packet, send)
+    # Stop spam command from sending to ourselves ourselves if spam count > 1
+    def onText(self, Client: Client, packet: Text, send: bool) -> (Text, bool):
+        if self.spamCount > 1 and packet.text[0:22] == "You have sent a gamble":
+            send = False
+        return (packet, send)
 
-	# Track players that have joined and left the map
-	def onUpdate(self, client: Client, packet: Update, send: bool) -> (Update, bool):
-		# Add players that join
-		for obj in packet.newObjects:
-			for classType in self.classTypes:
-				for s in obj.objectStatusData.stats:
-					if obj.objectStatusData.objectID != client.objectID and obj.objectType == classType and s.statType == 31:
-						self.players.append(obj)
-						if self.debug:
-							print("Added {} to list".format(s.strStatValue))
+    # Track players that have joined and left the map
+    def onUpdate(self, client: Client, packet: Update, send: bool) -> (Update, bool):
+        # Add players that join
+        for obj in packet.newObjects:
+            for classType in self.classTypes:
+                for s in obj.objectStatusData.stats:
+                    if obj.objectStatusData.objectID != client.objectID and obj.objectType == classType and s.statType == 31:
+                        self.players.append(obj)
+                        if self.debug:
+                            print("Added {} to list".format(s.strStatValue))
 
-		# Remove players that have left
-		for d in packet.drops:
-			for p in self.players:
-				if d == p.objectStatusData.objectID:
-					self.players.remove(p)
-					if self.debug:
-						for s in p.objectStatusData.stats:
-							if s.statType == 31:
-								print("Removed {} from list".format(s.strStatValue))
-		if self.debug:						
-			print(len(self.players))
-		return (packet, send)
+        # Remove players that have left
+        for d in packet.drops:
+            for p in self.players:
+                if d == p.objectStatusData.objectID:
+                    self.players.remove(p)
+                    if self.debug:
+                        for s in p.objectStatusData.stats:
+                            if s.statType == 31:
+                                print("Removed {} from list".format(
+                                    s.strStatValue))
+        if self.debug:
+            print(len(self.players))
+        return (packet, send)
 
-	# Few commands for trolling :trollface:
-	def onPlayerText(self, client: Client, packet: PlayerText, send: bool) -> (PlayerText, bool):
-		if packet.text[0:7] == "/debug ":
-			try:		
-				if packet.text.split(" ")[-1] == "on":
-					self.debug = True
-				elif packet.text.split(" ")[-1] == "off":  
-					self.debug = False
-				client.createNotification(self.pluginName, "Set Debug Mode to {}".format(self.debug))
-			except:
-				client.createNotification(self.pluginName, "Incorrect syntax. Use '/test ON | OFF'.")
-			send = False
+    # Few commands for trolling :trollface:
+    def onPlayerText(self, client: Client, packet: PlayerText, send: bool) -> (PlayerText, bool):
+        if packet.text[0:7] == "/debug ":
+            try:
+                if packet.text.split(" ")[-1] == "on":
+                    self.debug = True
+                elif packet.text.split(" ")[-1] == "off":
+                    self.debug = False
+                client.createNotification(
+                    self.pluginName, "Set Debug Mode to {}".format(self.debug))
+            except:
+                client.createNotification(
+                    self.pluginName, "Incorrect syntax. Use '/test ON | OFF'.")
+            send = False
 
-		if packet.text[0:8] == "/target ":	
-			self.target = packet.text.split(" ")[-1]
-			client.createNotification(self.pluginName, "Set Targete to {}".format(self.target))
-			send = False
+        if packet.text[0:8] == "/target ":
+            self.target = packet.text.split(" ")[-1]
+            client.createNotification(
+                self.pluginName, "Set Targete to {}".format(self.target))
+            send = False
 
-		if packet.text[0:8] == "/scount ":	
-			self.spamCount = int(packet.text.split(" ")[-1])
-			client.createNotification(self.pluginName, "Set count to {}".format(self.spamCount))
-			send = False	
+        if packet.text[0:8] == "/scount ":
+            self.spamCount = int(packet.text.split(" ")[-1])
+            client.createNotification(
+                self.pluginName, "Set count to {}".format(self.spamCount))
+            send = False
 
-		if packet.text[0:12] == "/sleepcount ":	
-			self.sleepCount = float(packet.text.split(" ")[-1])
-			client.createNotification(self.pluginName, "Set count to {}".format(self.sleepCount))
-			send = False		
+        if packet.text[0:12] == "/sleepcount ":
+            self.sleepCount = float(packet.text.split(" ")[-1])
+            client.createNotification(
+                self.pluginName, "Set count to {}".format(self.sleepCount))
+            send = False
 
-		if packet.text[0:8] == "/gamble ":
-			g = RequestGamble()	
-	
-			if packet.text.split(" ")[1] == "spam":
-				name = packet.text.split(" ")[-1]
-				g.amount = 0
-				if name == "all":
-					for p in self.players:
-						for s in p.objectStatusData.stats:
-							if s.statType == 31:
-								g.name = s.strStatValue
-						for i in range(self.spamCount):
-							client.SendPacketToServer(CreatePacket(g))	
-							time.sleep(self.sleepCount)
-					client.createNotification(self.pluginName, "Sent gamble request to {} players.".format(len(self.players)))	
-				else:
-					g.name = name
-					for i in range(self.spamCount):
-						client.SendPacketToServer(CreatePacket(g))
-						time.sleep(self.sleepCount)
-				client.createNotification(self.pluginName, "Sent gamble request to {} {} times!".format(name, i+1))
+        if packet.text[0:8] == "/gamble ":
+            g = RequestGamble()
 
-			elif packet.text.split(" ")[-1].isnumeric and int(packet.text.split(" ")[-1]) > 0:
-				name = packet.text.split(" ")[1]
-				amount = int(packet.text.split(" ")[-1])
-				g.name = name
-				g.amount = amount
-				client.createNotification(self.pluginName, "Sent gamble request to {} for {}.".format(g.name, g.amount))
-				client.SendPacketToServer(CreatePacket(g))
+            if packet.text.split(" ")[1] == "spam":
+                name = packet.text.split(" ")[-1]
+                g.amount = 0
+                if name == "all":
+                    for p in self.players:
+                        for s in p.objectStatusData.stats:
+                            if s.statType == 31:
+                                g.name = s.strStatValue
+                        for i in range(self.spamCount):
+                            client.SendPacketToServer(CreatePacket(g))
+                            time.sleep(self.sleepCount)
+                    client.createNotification(
+                        self.pluginName, "Sent gamble request to {} players.".format(len(self.players)))
+                else:
+                    g.name = name
+                    for i in range(self.spamCount):
+                        client.SendPacketToServer(CreatePacket(g))
+                        time.sleep(self.sleepCount)
+                client.createNotification(
+                    self.pluginName, "Sent gamble request to {} {} times!".format(name, i+1))
 
-			send = False	
+            elif packet.text.split(" ")[-1].isnumeric and int(packet.text.split(" ")[-1]) > 0:
+                name = packet.text.split(" ")[1]
+                amount = int(packet.text.split(" ")[-1])
+                g.name = name
+                g.amount = amount
+                client.createNotification(
+                    self.pluginName, "Sent gamble request to {} for {}.".format(g.name, g.amount))
+                client.SendPacketToServer(CreatePacket(g))
 
-		return (packet, send)
+            send = False
+
+        return (packet, send)
